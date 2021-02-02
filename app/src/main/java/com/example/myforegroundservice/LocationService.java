1221d729b114543c3ce8baddcd38983d8adb4596
rizwankarim <rizusamnani@gmail.com>
2021-02-02 11:31:27 +0500

    New Project

--- app/src/main/java/com/example/myforegroundservice/LocationService.java
+++ app/src/main/java/com/example/myforegroundservice/LocationService.java
@@ -6,6 +6,7 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.CountDownTimer;
 import android.os.IBinder;
 import android.os.Looper;
@@ -16,6 +17,7 @@ import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofencingClient;
 import com.google.android.gms.location.GeofencingRequest;
@@ -23,6 +25,7 @@ import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -31,6 +34,10 @@ import static com.example.myforegroundservice.App.CHANNEL_ID;
 
 public class LocationService extends Service {
 
+    private static final long UPDATE_INTERVAL_IN_MILLISECONDS = 3000;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private LocationRequest locationRequest;
+    private LocationSettingsRequest locationSettingsRequest;
     private GeofencingClient geofencingClient;
     private GeofenceHelper geofenceHelper;
     private String GEOFENCE_ID = "SOME_GEOFENCE_ID";
@@ -44,20 +51,32 @@ public class LocationService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         setNotification();
-        getCurrentLocation();
+        startLocationUpdates();
         return START_STICKY;
     }
 
-    public void setNotification(){
-        Intent myIntent= new Intent(this,MainActivity.class);
-        PendingIntent pendingIntent= PendingIntent.getActivity(this,0,myIntent,0);
-        Notification notification= new NotificationCompat.Builder(this,CHANNEL_ID)
+    private LocationCallback locationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            super.onLocationResult(locationResult);
+            Location currentLocation = locationResult.getLastLocation();
+            Log.d("Locations", currentLocation.getLatitude() + "," + currentLocation.getLongitude());
+            LatLng myLatlng= new LatLng(currentLocation.getLatitude(),currentLocation.getLongitude());
+            addGeofence(myLatlng,300);
+            //Share/Publish Location
+        }
+    };
+
+    public void setNotification() {
+        Intent myIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, myIntent, 0);
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
                 .setContentTitle("Location Foreground")
                 .setContentText("Your location is getting by application")
                 .setSmallIcon(R.drawable.ic_androidicon)
                 .setContentIntent(pendingIntent)
                 .build();
-        startForeground(1,notification);
+        startForeground(1, notification);
     }
 
     @Override
@@ -73,32 +92,19 @@ public class LocationService extends Service {
         return null;
     }
 
-    public void getCurrentLocation() {
-        LocationRequest locationRequest = new LocationRequest();
-        //locationRequest.setInterval(3000);
-        //locationRequest.setFastestInterval(5000);
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    private void startLocationUpdates() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
             return;
         }
-        LocationServices.getFusedLocationProviderClient(this)
-                .requestLocationUpdates(locationRequest, new LocationCallback() {
-                    @Override
-                    public void onLocationResult(LocationResult locationResult) {
-                        super.onLocationResult(locationResult);
-                        LocationServices.getFusedLocationProviderClient(getApplicationContext())
-                                .removeLocationUpdates(this);
-                        if (locationResult != null && locationResult.getLocations().size() > 0) {
-                            int locationindex = locationResult.getLocations().size() - 1;
-                            double current_lat = locationResult.getLocations().get(locationindex).getLatitude();
-                            double current_long = locationResult.getLocations().get(locationindex).getLongitude();
-                            LatLng myLatlng = new LatLng(current_lat, current_long);
-                            addGeofence(myLatlng,200);
-
-                            Log.d("Receiver", String.valueOf(current_lat) + "," + String.valueOf(current_long));
-                        }
-                    }
-                }, Looper.getMainLooper());
+        mFusedLocationClient.requestLocationUpdates(this.locationRequest,
+                this.locationCallback, Looper.myLooper());
     }
 
     public void addGeofence(LatLng latLng, float radius) {
@@ -128,6 +134,10 @@ public class LocationService extends Service {
     }
 
     public void initData(){
+        locationRequest = LocationRequest.create();
+//        locationRequest.setInterval(UPDATE_INTERVAL_IN_MILLISECONDS);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         geofencingClient = LocationServices.getGeofencingClient(this);
         geofenceHelper = new GeofenceHelper(this);
     }
